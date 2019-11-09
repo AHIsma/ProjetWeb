b180ef649bd2a0aac0e6f45ff02885dcb0bc07c2
amineoubaya1921 <amineoubaya1921@gmail.com>
2019-11-09 13:16:34 +0100

    Ajout de la page principal du site

---
+++ bdecesicampus.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="icon" type="image/png" href="favicon.png" />
+	<title>BDE CESI REIMS</title>
+</head>
+
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">BDE CESI École d’Ingénieurs REIMS</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav ml-auto">
+					<a class="nav-item nav-link" href="#">S'INSCRIRE</a>
+					<a class="nav-item nav-link" href="#">SE CONNECTER</a>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<aside class="image_soiree_design">
+			<img class="image_soiree" src="assets/images/image_soirée_BDE_CESI_REIMS.png"
+				alt="Image d'un evenement BDE" />
+		</aside>
+	</main>
+	<footer>
+		<p>Suivez-nous !</p>
+		<ul class="sociaux">
+		<li><a href="https://www.qries.com/"><img class="fb" src="assets/images/ico_facebook.png" /> </a></li>
+		<li><img class="insta" src="assets/images/ico_instagram.png" /></li>
+		<li><img class="youtube" src="assets/images/ico_youtube.png" /></li>
+		</ul>
+	</footer>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+	integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+	integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+<script src="https://code.jquery.com/jquery-1.12.3.js" integrity="sha256-1XMpEtA4eKXNNpXcJ1pmMPs8JV+nwLdEqwiJeCQEkyc="
+	crossorigin="anonymous"></script>
+<script src="assets/js/javaS.js"></script>
+
+</html>
