3602126415b53a29a8838e8e1313bcfea0a44c98
amineoubaya1921 <amineoubaya1921@gmail.com>
2019-11-14 23:18:59 +0100

    Changement des liens dans les navbar

--- nav_connecte.php
+++ nav_connecte.php
@@ -33,7 +33,7 @@
         </div>
         <div class="nav_all">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="boutique_connecte.php"></a>
+                <a class="navbar-brand" href="boutique.php"></a>
                 <button class="navbar-toggler ml-auto hidden-sm-up float-xs-right" type="button" data-toggle="collapse"
                     data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
                     aria-label="Toggle navigation">
