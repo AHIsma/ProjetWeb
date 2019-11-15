7c31ab802d8a26d6289e696aaa1571b92b2cf35e
Dgouss <50356290+Dgouss@users.noreply.github.com>
2019-11-15 14:55:07 +0100

    rien de spécial

--- requests/scriptAjoutPanier_bdd.php
+++ requests/scriptAjoutPanier_bdd.php
@@ -3,14 +3,26 @@ if(!isset($_SESSION)){
     session_start();}
 
 include 'connexionBdd.php';
-$requete = $bdd->prepare("INSERT INTO paniers SELECT ET.Id as Id_user, ET.Nom as Nom, ET.Prenom as Prenom, ET.Email as email, ev.ID as Id_evenement, ev.Designation as Designation, ev.Prix as Prix, ev.Images as Images
-FROM etudiantinscrit ET INNER JOIN evenement ev
-WHERE `et`.`Id` =:Id_user AND `ev`.`ID`=:Id_evenement");
-        $requete->bindvalue(':Id_user', $_SESSION['Id_user'], PDO::PARAM_STR);
-        $requete->bindvalue(':Id_evenement', $_GET['id_evenement'], PDO::PARAM_STR);
-        $requete->execute();
-        echo "article ajouté au panier";
 
+$requete = $bdd->prepare("SELECT * FROM paniers WHERE `Id_evenement`=:Id_evenement;");
+    // Liaison des variables de la requête préparée aux variables PHP
+		$requete->bindvalue(':Id_evenement', $_GET['id_evenement'], PDO::PARAM_STR);
+
+        $requete->execute();
+
+    if (!$requete->fetch()){
+
+		$requete = $bdd->prepare("INSERT INTO paniers SELECT ET.Id as Id_user, ET.Nom as Nom, ET.Prenom as Prenom, ET.Email as email, ev.ID as Id_evenement, ev.Designation as Designation, ev.Prix as Prix, ev.Images as Images
+		FROM etudiantinscrit ET INNER JOIN evenement ev
+		WHERE `et`.`Id` =:Id_user AND `ev`.`ID`=:Id_evenement");
+		$requete->bindvalue(':Id_user', $_SESSION['Id_user'], PDO::PARAM_STR);
+		$requete->bindvalue(':Id_evenement', $_GET['id_evenement'], PDO::PARAM_STR);
+		$requete->execute();
+		echo "article ajouté au panier";
+	}
+	else {
+    	echo"déjà inscrit à l'évènement";
+    }
 
 ?>
 
--- requests/scriptInscriptionEvenement.php
+++ requests/scriptInscriptionEvenement.php
@@ -3,15 +3,25 @@ if(!isset($_SESSION)){
     session_start();}
 
 include 'connexionBdd.php';
-$requete = $bdd->prepare("INSERT INTO inscritevenement SELECT et.Id as Id_user, et.Nom as Nom, et.Prenom as Prenom, ev.ID as Id_evenement, ev.Designation as Designation
-FROM etudiantinscrit et INNER JOIN evenement ev
-WHERE `et`.`Id` =:Id_user AND `ev`.`ID`=:Id_evenement");
-        $requete->bindvalue(':Id_user', $_SESSION['Id_user'], PDO::PARAM_STR);
-        $requete->bindvalue(':Id_evenement', $_GET['id_evenement'], PDO::PARAM_STR);
+
+$requete = $bdd->prepare("SELECT * FROM inscritevenement WHERE `id_evenement`=:Id_evenement;");
+    // Liaison des variables de la requête préparée aux variables PHP
+		$requete->bindvalue(':Id_evenement', $_GET['id_evenement'], PDO::PARAM_STR);
+
         $requete->execute();
-        echo "article ajouté";
 
+    if (!$requete->fetch()){
+		$requete = $bdd->prepare("INSERT INTO inscritevenement SELECT et.Id as Id_user, et.Nom as Nom, et.Prenom as Prenom, ev.ID as Id_evenement, ev.Designation as Designation
+		FROM etudiantinscrit et INNER JOIN evenement ev
+		WHERE `et`.`Id` =:Id_user AND `ev`.`ID`=:Id_evenement");
+		$requete->bindvalue(':Id_user', $_SESSION['Id_user'], PDO::PARAM_STR);
+		$requete->bindvalue(':Id_evenement', $_GET['id_evenement'], PDO::PARAM_STR);
+		$requete->execute();
+		echo "inscription ajouté";
+    }
+    else {
+    	echo"déjà inscrit à l'évènement"# code...
+    }
 ?>
-
-<br>
-<a href="../activites.php">revenir aux activités </a>
+    <br>
+		<a href="../activites.php">revenir aux activités </a>
